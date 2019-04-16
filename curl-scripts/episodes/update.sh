a0a2987637ae0ee67bc64e0a387cb3135ff859c9
fritzable <fritzzillad@aol.com>
2019-04-16 17:30:20 -0400

    Add successful update curl script
    - Test PATCH route with curl script successfully

--- curl-scripts/episodes/update.sh
+++ curl-scripts/episodes/update.sh
@@ -4,15 +4,19 @@ API="http://localhost:4741"
 URL_PATH="/episodes"
 URL="https://longreadsblog.files.wordpress.com/2019/04/dp1-lg.jpg"
 
-curl "${API}${URL_PATH}/5cb5ee051c5b1f349e6af430" \
+curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
---header "Authorization: Bearer ${TOKEN}" \
---data '{
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
     "episode": {
-      "urls": "https://longreadsblog.files.wordpress.com/2019/04/dp1-lg.jpg"
-    }
+      "owner": "'"${OWNER}"'",
+      "urls": [
+      {"url": "https://longreadsblog.files.wordpress.com/2019/04/dp1-lg.jpg"},
+      {"url": "https://longreadsblog.files.wordpress.com/2019/04/jenny-odell-orb-of-ambivalence-1.png"}
+    ]
+  }
   }'
 
 echo
